ff7278f247b98f3f181ad1f7843f671621f1f27d
Muhammed Osman <91621437+muh-osman@users.noreply.github.com>
2023-06-12 21:54:22 +0300

    Update NamesProvider.js

--- src/context/NamesProvider.js
+++ src/context/NamesProvider.js
@@ -1,24 +1,18 @@
 // useContext
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
 
-
-
-
-export const NamesContext = createContext()
-
+export const NamesContext = createContext();
 
 export default function NamesProvider(props) {
-
-const [names, setNames] = useState({
-    name: "Muhammed"
-})
+  const [names, setNames] = useState({
+    name: "Muhammed",
+  });
 
   return (
     <>
-    <NamesContext.Provider value={names}>
+      <NamesContext.Provider value={{ names, setNames }}>
         {props.children}
-    </NamesContext.Provider>
-    
+      </NamesContext.Provider>
     </>
-  )
+  );
 }
